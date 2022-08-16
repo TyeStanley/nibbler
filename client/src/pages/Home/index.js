d6788cbf3a877fd6fa267a2747fc50f6280c459c
Azarill <heierms@gmail.com>
2022-08-16 01:28:56 -0500

    layed foundation for homepage

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,18 +1,45 @@
 import './index.scss';
+import images from './seeds'
+
 
 const Home = () =>{
 
     return(
         <>
-        <section className ="p-5 bg-primary text-white text-center" id="hero-section">
+        <section  className ="p-5 bg-primary text-white text-center" id="hero-section"></section>
+
+         <section className ="text-center pt-3" id='intro-statement'>
+            <div className='intro-titles'>
+                <div className='intro-div'><h1 className> Welcome to "PlaceHolder"</h1></div>
+                <h2 className= 'pt-2 pb-3'> Save your favorite spots | Share them with friends | Find something new</h2>
+            </div>
+            <h3 className='p-3'> Recent Uploads </h3>
+         </section>
+
+         <section  className='intro-restaurants' id="intro-restaurants">
+
+            <div className='top-five-recent container-fluid text-center  d-flex flex-wrap'>
+                {images.map(({name, link}) =>(
+                    
+                        <div className='restPhotosDiv col-sm-12 col-md-3'>
+                            <div className='p-1'>
+                                <h4 className='col-12'>{name}</h4>
+                                <img src={link} className='col-12' alt={name}></img>
+                            </div>
+                        </div>
+
+
+
+                ))}
+                
+              
+
+            </div>
             
-            <h1 className="pt-5"> Welcome to "PlaceHolder" </h1>
-
+            
 
          </section>
-         <section className ="p-5 text-center" id='intro-statement'>
-            <h2> Save your favorite spots | Share them with friends | Find something new</h2>
-         </section>
+
       </>
 
     );
---
+++ client/src/pages/Home/seeds/index.js
@@ -0,0 +1,35 @@
+module.exports = [
+
+    {
+        "name":"img-hold",
+        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+
+    },
+    {
+        "name":"img-hold",
+        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+
+    },
+    {
+        "name":"img-hold",
+        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+
+    },
+    {
+        "name":"img-hold",
+        "link": "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"
+
+    },
+    {
+        "name":"img-hold",
+        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+
+    },
+
+
+
+
+
+
+
+]

a2dd14b47888a8a674af373284c9de79176078c2
Azarill <heierms@gmail.com>
2022-08-16 15:44:34 -0500

    finished homepage layout

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,41 +1,63 @@
 import './index.scss';
 import images from './seeds'
+import UserReview from '../../components/UserReview';
 
 
 const Home = () =>{
 
     return(
         <>
-        <section  className ="p-5 bg-primary text-white text-center" id="hero-section"></section>
+        <section  className ='p-5 bg-primary text-white text-center' id='hero-section'></section>
 
-         <section className ="text-center pt-3" id='intro-statement'>
+         <section className ='text-center pt-3' id='intro-statement'>
             <div className='intro-titles'>
-                <div className='intro-div'><h1 className> Welcome to "PlaceHolder"</h1></div>
-                <h2 className= 'pt-2 pb-3'> Save your favorite spots | Share them with friends | Find something new</h2>
+                <div className='intro-div'><h1 id='intro-h1'> Welcome to "PlaceHolder"</h1></div>
+                <h2 className= 'pt-2 pb-3' id='intro-h2'> Save your favorite spots | Share them with friends | Find something new</h2>
             </div>
-            <h3 className='p-3'> Recent Uploads </h3>
+            <h3 className='p-3' id='recentUploads'> Recent Uploads </h3>
          </section>
 
-         <section  className='intro-restaurants' id="intro-restaurants">
+         <section  id="intro-restaurants" className='border-primary d-flex flex-wrap'>
 
-            <div className='top-five-recent container-fluid text-center  d-flex flex-wrap'>
-                {images.map(({name, link}) =>(
+            <div className=' col-12 col-md-8 text-center d-flex flex-wrap' id='recent-uploads-div'>
+                {images.map(({name, imgLinks}) =>(
                     
-                        <div className='restPhotosDiv col-sm-12 col-md-3'>
-                            <div className='p-1'>
-                                <h4 className='col-12'>{name}</h4>
-                                <img src={link} className='col-12' alt={name}></img>
+                        <>
+                        <div className="card col-12 col-md-12 m-1 border border-dark ">
+                            <h5 className="card-header" id='rest-card-header'>{name}</h5>
+                            <div className="" id='restCardBody'>
+                              <div id='card-img-container' className='d-flex carousel slide' data-ride='carousel'>  
+                              {imgLinks.map((link) => (
+                                <img src={link} className='col-8 col-md-4 p-1' alt={name} id='card-img'></img>
+                              ))}
+                              </div>   
+    
                             </div>
                         </div>
-
-
+                        
+                       </>
 
                 ))}
                 
               
 
             </div>
+            {/* <div className='col-12 col-md-4 mt-1 p-3 d-flex flex-wrap ' id='desc-box'>
+             {images.map(({userName,userPhoto,review}) =>(
+                <>
+                    <div className='d-flex p-1 col-12' id='desc-box-title'>
+                        <img  src={userPhoto} className='col-2' id='userCommentImg' alt={"avatar image for " + userName} ></img>
+                        <p className='col-4 p-3'>{userName}</p>
+                    </div>
+
+                    <p id="reviewText" className='col-12'>" {review} "</p>
+
+                </>
+             ))}                   
             
+            </div> */}
+            
+            <UserReview></UserReview>
             
 
          </section>
--- client/src/pages/Home/seeds/index.js
+++ client/src/pages/Home/seeds/index.js
@@ -1,30 +1,50 @@
 module.exports = [
 
     {
-        "name":"img-hold",
-        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        "name":"Restaurant",
+        "imgLinks":["https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"], 
+        "userName": "UserName ",
+        "userPhoto":"https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg",
+        "review": "We loved this place it was sooooo good! We loved this place it was sooooo good! We loved this place it was sooooo good! We loved this place it was sooooo good! We loved this place it was sooooo good!"
 
     },
     {
-        "name":"img-hold",
-        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        "name":"Restaurant",
+        "imgLinks":["https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"],
+        "userName": "UserName ",
+        "userPhoto":"https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg",
+        "review": "Hello" 
 
     },
     {
-        "name":"img-hold",
-        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-
+        "name":"Restaurant",
+        "imgLinks":["https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80","https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80","https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80","https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"], 
+        "userName": "UserName ",
+        "userPhoto":"https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg",
+        "review": "Hello"
     },
     {
-        "name":"img-hold",
-        "link": "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"
-
+        "name":"Restaurant",
+        "imgLinks":["https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"], 
+        "userName": "UserName ",
+        "userPhoto":"https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg",
+        "review": "Hello"
     },
     {
-        "name":"img-hold",
-        "link": "https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-
+        "name":"Restaurant",
+        "imgLinks":["https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"], 
+        "userName": "UserName ",
+        "userPhoto":"https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg",
+        "review": "Hello"
     },
+    {
+        "name":"Restaurant",
+        "imgLinks":["https://images.unsplash.com/photo-1578687388049-079580e6eb2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"], 
+        "userName": "UserName ",
+        "userPhoto":"https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg",
+        "review": "Hello"
+    }
+
 
 
 

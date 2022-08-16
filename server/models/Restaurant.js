66fcde7f39fd267c7b1f495ab8fcf794acaa3236
SD-github21 <sapandonde.gmail.com>
2022-08-15 22:07:06 -0500

    added code to Restaurant model

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -1,22 +1,51 @@
 const { Schema, model } = require('mongoose');
 
-const RestaurantSchema = new Schema ({
+const RestSchema = new Schema ({
     restName: {
         type: String,
         required: true
     },
     createdBy: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     }, 
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     dishName: {
+        type: String,
+        required: true
+    },
+    restCity: {
         type: String
     },
-    
+    restState: {
+        type: String
+    },
+    dishCost: {
+        type: Number
+    },
+    foodPhoto: [],
+    foodDescript: {
+        type: String
+    },
+    comments: {
+        type: String
+    }
+  }, 
+  {
+    toJSON: {
+        getters: true
+    },
+    id: false
+  }
+);
 
+// Create the Restaurant model using RestSchema
+const Rest = model('Rest', RestSchema);
 
-
-})
+// Export the Restaurnt model
+module.exports = Rest;

d58972a6ecbe6ecbe5e8b24ee6acbf7c0c6240d0
Azarill <heierms@gmail.com>
2022-08-15 22:57:30 -0500

    orginized files | setup begining components | install packages

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import './index.scss';
+
+const Home = () =>{
+
+    return(
+        <>
+        <section className ="p-5 bg-primary text-white text-center" id="hero-section">
+            
+            <h1 className="pt-5"> Welcome to "PlaceHolder" </h1>
+
+
+         </section>
+         <section className ="p-5 text-center" id='intro-statement'>
+            <h2> Save your favorite spots | Share them with friends | Find something new</h2>
+         </section>
+      </>
+
+    );
+};
+
+
+
+export default Home;

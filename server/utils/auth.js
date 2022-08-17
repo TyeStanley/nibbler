f29d466670696abe3b0f460be8f3eacc9f7f86e1
SD-github21 <sapandonde.gmail.com>
2022-08-17 15:27:59 -0500

    added authentication functionality

---
+++ server/utils/auth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
+
+const expiration = '2h';
+
+module.exports = {
+    signToken: function({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+
+    authMiddleware: function({ req }) {
+        // Allows tokens to be sent via req.body, req.query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // Separate "Bearer" from "<tokenvalue>"
+        if (req.headers.authorization) {
+            token = token
+              .split(' ')
+              .pop()
+              .trim();
+        }
+
+        // If no token, return request object as is
+        if (!token) {
+            return req;
+        }
+
+        try {
+            // Decode and attach user data to request object
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        // Return updated request object
+        return req;
+    }
+};

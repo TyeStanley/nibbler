e993557c3ea01805c306b18fa561510261ce3445
CMiller <mlr.chase40@gmail.com>
2022-08-17 14:34:52 -0500

    add progress to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,27 @@
+const { User, Rest } = require("../models");
 
+const resolvers = {
+  Query: {
+    users: async () => {
+      return await User.find();
+    },
+    user: async (parent, { username }) => {
+      return await User.findOne({ username }).select("-__v -password");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+    addRest: async (parent, args) => {
+        const rest = await Rest.create(args);
+    
+        return rest;
+    }
+  },
+};
+
+module.exports = resolvers;

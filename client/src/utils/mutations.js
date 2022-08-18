4d7fafa057bd1bae69d7ffd7d8ce6de3a8ada243
SD-github21 <sapandonde.gmail.com>
2022-08-17 23:44:43 -0500

    added auth, mutations, and queries files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                tagline
+                profilePic
+            }
+        }
+    }
+`;
+
+export const ADD_REST = gql`
+    mutation addRest($restName: String!, $restState: String!, $restCity: String!, $restAddress: String!, $restDescript: String!) {
+        addRest(restName: $restName, restState: $restState, restCity: $restCity, restAddress: $restAddress, restDescript: $restDescript) {
+        _id
+        restName
+        restState
+        restCity
+        restAddress
+        restDescript
+        restPhotos
+        dishes {
+            _id
+            dishName
+            dishPhotos
+        }
+        comments {
+            _id
+            commentBody
+        }
+    }
+}`
+

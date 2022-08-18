f433d8234aea00ab91ed4de7b3d0c61dd0ed91ac
SD-github21 <sapandonde.gmail.com>
2022-08-17 23:58:20 -0500

    added queries code

---
+++ client/src/utils/queries.js
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql `
+    query me($_id: ID!) {
+        me(_id: $id) {
+            _id
+            username
+            tagline
+            profilePic
+            favRests {
+                _id
+                restName
+                createdAt
+                restPhotos {
+                    _id
+                    photoUrl
+                    userId
+                }
+                comments {
+                    _id
+                    commentBody
+                    createdAt
+                    heartsCount
+                }
+                dishes {
+                    _id
+                    dishName
+                    dishCost
+                    dishPhotos
+                }
+
+            }
+        }
+    }
+`;
+

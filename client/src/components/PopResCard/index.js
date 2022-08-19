6e7bc94806d4746c9000b42ca51f718e5b107e03
Azarill <heierms@gmail.com>
2022-08-18 22:03:40 -0500

    updated styling

--- client/src/components/PopResCard/index.js
+++ client/src/components/PopResCard/index.js
@@ -5,11 +5,14 @@ const PopResCard = ({imgLinks,name}) => {
         <>  
         <div className id='pop-res-container'>
 
-             
+        
              <img src={imgLinks} alt={name} id='pop-res-img'>
 
             </img>
+            
             <h2 id='pop-res-h2'>{name}</h2> 
+            <div className id='pop-res-bottom'> <i id='heart-svg' className="fa-solid fa-heart p-1 mx-2"></i><span id='heart-span'>34000</span></div>
+
 
         </div>
         </>

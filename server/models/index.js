cac8e8530e9a52787ea82368cf2c5f93e68e981e
SD-github21 <sapandonde.gmail.com>
2022-08-18 22:39:12 -0500

    tested out deleting merge conflict

--- server/models/index.js
+++ server/models/index.js
@@ -2,13 +2,7 @@ const User = require('./User');
 const Rest = require('./Restaurant');
 const Comment = require('./Comment');
 const Dish = require('./Dish');
-<<<<<<< HEAD
 const Photo = require('./Photo');
 const Heart = require('./Heart');
 
 module.exports = { User, Rest, Comment, Dish, Photo, Heart };
-=======
-const Heart = require('./Heart');
-
-module.exports = { User, Rest, Comment, Dish, Heart };
->>>>>>> server

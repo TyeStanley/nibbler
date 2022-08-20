e76cb4933102333be1079a924587ad77a733f2f7
Azarill <heierms@gmail.com>
2022-08-19 21:37:34 -0500

    update quieries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,72 @@
+/** @format */
+
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        tagline
+        profilePic
+      }
+    }
+  }
+`;
+
+export const ADD_REST = gql`
+  mutation addRest(
+    $restName: String!
+    $restState: String!
+    $restCity: String!
+    $restAddress: String!
+    $restDescript: String!
+  ) {
+    addRest(
+      restName: $restName
+      restState: $restState
+      restCity: $restCity
+      restAddress: $restAddress
+      restDescript: $restDescript
+    ) {
+      _id
+      restName
+      restState
+      restCity
+      restAddress
+      restDescript
+      restPhotos {
+        _id
+        photoUrl
+        userId
+      }
+      dishes {
+        _id
+        dishName
+        dishPhotos {
+          _id
+          photoUrl
+          userId
+        }
+      }
+      comments {
+        _id
+        commentBody
+      }
+    }
+  }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,40 @@
+/** @format */
+
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query me($_id: ID!) {
+    me(_id: $id) {
+      _id
+      username
+      tagline
+      profilePic
+      favRests {
+        _id
+        restName
+        createdAt
+        restPhotos {
+          _id
+          photoUrl
+          userId
+        }
+        comments {
+          _id
+          commentBody
+          createdAt
+          heartsCount
+        }
+        dishes {
+          _id
+          dishName
+          dishCost
+          dishPhotos {
+            _id
+            photoUrl
+            userId
+          }
+        }
+      }
+    }
+  }
+`;

fffa2a72f693e6f2b925e4eecba2ba48c2d1a0a7
macpat83 <macpat@gmail.com>
2022-08-20 08:20:33 -0500

    cleaned up code

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,38 +24,3 @@ export const ADD_USER = gql`
   }
 `;
 
-export const SAVE_BOOK = gql`
-  mutation saveBook($book: SavedBookInput!) {
-    saveBook(book: $book) {
-      username
-      email
-      bookCount
-      savedBooks {
-        authors
-        description
-        bookId
-        image
-        link
-        title
-      }
-    }
-  }
-`;
-
-export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: String!) {
-    removeBook(bookId: $bookId) {
-      username
-      email
-      bookCount
-      savedBooks {
-        authors
-        description
-        bookId
-        image
-        link
-        title
-      }
-    }
-  }
-`;

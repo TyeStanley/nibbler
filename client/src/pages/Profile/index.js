61e4a460c915a8f6c08b6dd6bddc43bb4965847a
macpat83 <macpat@gmail.com>
2022-08-20 23:40:25 -0500

    fixed buttons

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,45 +1,32 @@
-import React, { useState, useEffect, useQuery } from 'react';
+import './index.scss'
+import React, { useState } from 'react';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
-import { Nav, Modal, Tab, Button, Container, CardGroup, Card } from 'react-bootstrap';
+import { Modal, Tab, Button } from 'react-bootstrap';
 import RestaurantCard from '../../components/RestaurantCard';
-import Auth from '../../utils/auth';
 import AddRestaurant from '../../components/AddRestaurant';
-import { Form,  Alert } from 'react-bootstrap';
+import EditProfile from '../../components/EditProfile';
 import { useQuery } from '@apollo/client';
-import { QUERY_RESTAURANTS } from '../../utils/queries';
+import { QUERY_ME } from '../../utils/queries';
 
 const Profile= () => {
   const [showModal, setShowModal] = useState(false);
-  const [userData, setUserData] = useState({});
-  const { data } = useQuery(GET_ME)
+  const [showModal2, setShowModal2] = useState(false);
+  const { data } = useQuery(QUERY_ME)
+
+
+  let faveRest =  data?.me.favRests;
+  let mostFave ='';
+
+  if(faveRest?.length > 8){
+    const newFave = [...faveRest];
+
+     mostFave = [...faveRest];
+    faveRest = newFave.splice(-8);
+
+}
+else{  faveRest = data?.me.favRests};
+
   
-  const userDataLength = Object.keys(userData).length;
-  let restaurantslist =  data?.restaurants;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
   
     return (
 
@@ -48,13 +35,34 @@ const Profile= () => {
         <MDBRow className="justify-content-center align-items-center h-100">
           <MDBCol lg="12" xl="10">
             <MDBCard>
-              <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#2C2f31fd', height: '200px' }}>
+              <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#2C2f31fd', height: '250px' }}>
                 <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
                   <MDBCardImage src="https://ca-times.brightspotcdn.com/dims4/default/46e9fc6/2147483647/strip/true/crop/3500x1969+0+0/resize/840x473!/format/webp/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fa3%2Fbc%2F967153f7443ab69d6b182b91b1f4%2Ffo-quickbites-07102022-02.jpg"
-                    alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
-                  <MDBBtn outline color="dark" style={{height: '36px', overflow: 'visible'}}>
+                    alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '200px', zIndex: '1' }} />
+                  <MDBBtn onClick={() => setShowModal2(true)} color="dark"  style={{height: '36px', overflow: 'visible'}}>
                     Edit profile
                   </MDBBtn>
+                  <Modal 
+                  
+              
+                  size='lg'
+        show={showModal2}
+        onHide={() => setShowModal2(false)}
+        aria-labelledby='edituser-modal'>
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey='edituser'>
+          <Modal.Header closeButton>
+            
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey='edituser'>
+                <EditProfile handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+          </Tab.Container>
+      </Modal>
                 </div>
                 <div className="ms-3" style={{ marginTop: '130px' }}>
                   <MDBTypography tag="h5">McKinley Wiltz</MDBTypography>
@@ -85,14 +93,13 @@ const Profile= () => {
 
       <section class = 'container d-flex flex-row justify-content-between'>
       <div className='col-12 col-md-8 text-center d-flex flex-wrap' id='recent-uploads-div'>
-          <RestaurantCard restaurants={restaurantslist}></RestaurantCard>
+        <h2>Favorite Restaurants</h2>
+          {mostFave && <RestaurantCard faveRest={mostFave}></RestaurantCard>}
         </div>
         <div>
-      
-      
-                <Button  onClick={() => setShowModal(true)} outline color="dark" style={{height: '36px', overflow: 'visible'}}>
+      <MDBBtn  color= "dark" style={{height: '36px', overflow: 'visible'}} onClick={() => setShowModal(true)}>
                     Add Restaurant
-              </Button>
+              </MDBBtn>
             </div>
             <Modal
         size='lg'

5900b6f49703b7633363ed3b2335ead3296e8418
Azarill <heierms@gmail.com>
2022-09-27 23:32:36 -0500

    continued work on adding heart feature

--- client/src/components/RestaurantCard/index.js
+++ client/src/components/RestaurantCard/index.js
@@ -18,7 +18,7 @@ const RestaurantCard = ({restaurants}) =>{
   const loggedIn = Auth.loggedIn();
   // pull user data
   const { data } = useQuery(QUERY_ME)
-  const userData =  data?.me;
+  let userData =  data?.me;
   let restState = restaurants;
 
   const [addHeart] = useMutation(ADD_HEART);
@@ -34,7 +34,8 @@ const RestaurantCard = ({restaurants}) =>{
       setCurrentRests(restState);
     }
 
-  },[restState,userData])
+
+  },[restState])
 
 
 
@@ -42,21 +43,33 @@ const RestaurantCard = ({restaurants}) =>{
   // click handler for when hearts are clicked
   const heartClickHandler = (e) =>{
     let restToSave = e.target.getAttribute("data-id");
-     
+    setUser({...userData});
+    console.log(currentUser)
+    console.log(currentRests)
     // check if heart exists in the users favorite restaurant if it does remove it from the restaurant and the user
-      if(currentUser.favRests.indexOf(restToSave) != -1){
-        const keyToRemove = currentUser.favRests.indexOf(restToSave);
-
+      if(currentUser.favRests.map(rest => rest._id === restToSave)){
+        console.log('hello true')
           try{
             removeHeart({
-              variables: {restId: restToSave}
+              variables: {heartId: restToSave}
             })
           }
             catch(err){
               console.error(err);
             }
 
-         return setUser({...currentUser, favRests: currentUser.favRests.pull(keyToRemove)})
+            let newFavRests = currentUser.favRests.filter(favRest => favRest._id !== restToSave);
+            console.log(newFavRests)
+
+            // setCurrentRests(restState.map(rest=> {
+            //   if(rest._id === restToSave){
+            //     return {...rest, favRests: newFavRests}
+            //   }
+            //   else{
+            //     return rest
+            //   }
+            // }))
+         return setUser({...currentUser, favRests: newFavRests})
       }
       // if heart does not exists add to users favorite restaurant and hearts 
         try {
@@ -68,7 +81,10 @@ const RestaurantCard = ({restaurants}) =>{
       } catch (err) {
         console.error(err);
       }
-  
+      let favRestArray = [...currentUser.favRests, {_id: restToSave}];
+      console.log("This is the fav array " + favRestArray);
+      
+      return setUser({...currentUser, favRests: favRestArray})
 
     }
 
@@ -85,9 +101,6 @@ const handleTestBtn = (e) =>{
 
     
 }
-  if(userData){
-    console.log(userData)
-  }
 if(currentRests){
   return(
         <>      
@@ -103,7 +116,14 @@ if(currentRests){
                                         {loggedIn && <PhotoForm restId={_id} key={restName + "hoah123"}> </PhotoForm>} 
                                     </div>   
                               
-                                {/* {savedFavRestIds?.some((savedId) =>  savedId === _id) ? <div  id='pop-res-bottom' data-id={_id} onClick={heartClickHandler}> <i id='heart-svg' data-id={_id}  className={`fa-solid fa-heart p-2 mx-1 pink-heart`} key={restName}></i><span id='heart-span'>{`${heartsCount +1}`}</span></div> : <div  id='pop-res-bottom' data-id={_id} onClick={heartClickHandler}> <i id='heart-svg' data-id={_id}  className={`fa-solid fa-heart p-2 mx-1`} key={restName}></i><span id='heart-span'>{`${heartsCount}`}</span></div>} */}
+                                {heartsCount ? <div  id='pop-res-bottom' data-id={_id} onClick={heartClickHandler}> 
+                                                  <i id='heart-svg' data-id={_id}  className={`fa-solid fa-heart p-2 mx-1 pink-heart`} key={restName}></i>
+                                                  <span id='heart-span'>{`${heartsCount}`}</span></div> 
+                                                : 
+                                                <div  id='pop-res-bottom' data-id={_id} onClick={heartClickHandler}> 
+                                                  <i id='heart-svg' data-id={_id}  className={`fa-solid fa-heart p-2 mx-1`} key={restName}></i>
+                                                  <span id='heart-span'>{`${heartsCount}`}</span>
+                                               </div>}
                                 {comments && <UserReview comments={comments}  ></UserReview>}
                                 {loggedIn && (
                                   
